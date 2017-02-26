c2aa79022677fec5e2319c207f8a32792b54347f
Vamskazhi <vamskazi@gmail.com>
2017-02-26 08:26:36 +0700

    Delete topars.py

--- topars.py
+++
@@ -1,8 +0,0 @@
-print
-soup.title  # <title>Python 3 - Программирование на Python 3</title>
-print
-soup.title.string  # Python 3 - Программирование на Python 3
-
-# Содержимое мета полей
-for meta in soup.find_all('meta'):
-    print(meta.get('content'))
